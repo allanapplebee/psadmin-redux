29d00a586acc34a782bb72d7b52d49864c059c78
Gaby Saunders <allan.applebee@gmail.com>
2018-07-25 08:20:22 +1000

    thunk and mock api added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
 import reduxImmutableInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableInvariant())
+    applyMiddleware(thunk, reduxImmutableInvariant())
   );
 }
